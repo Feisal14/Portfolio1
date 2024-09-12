76e29ad0decd06a63f0f716f73b5b814042abdb2
Firu2 <ixcombo@hotmail.com>
2024-09-12 22:19:37 +0300

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar/>
       <div className="bg-red-400">
         <h1 className="text-4xl text-center text-white">Hello World</h1>
       </div>
