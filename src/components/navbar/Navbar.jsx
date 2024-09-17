439724fb78786bcf31da20ffb4d6dec7fc8695bf
Ffiru <frass550000@hotmail.com>
2024-09-17 18:54:37 +0300

    final update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,46 @@
-import { navItems } from "../../../data";
 import { useState } from "react";
+import { navItems } from "../../../data";
 import { FaAlignLeft } from "react-icons/fa6";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
+  const [dropdown, setDropdown] = useState(false);
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
   };
+  useGSAP(() => {
+    if (dropdown == true) gsap.to("#dropdown", { x: -20 });
+    else {
+      gsap.to("#dropdown", { x: -42 });
+    }
+  }, [dropdown]);
   return (
     <div className="mx-16 my-6 py-6 sm:mx-4 sm:my-2">
       <div className="flex md:justify-between items-center lg:justify-between sm:justify-between  sm:items-center ">
-        <button className="navitems hidden md:hidden sm:flex">
+        <button
+          className="navitems hidden md:hidden sm:flex "
+          onClick={handleDropdown}
+        >
           <FaAlignLeft />
+          <div id="dropdown" className="-translate-x-9">
+            {dropdown && (
+              <span className="flex flex-col absolute -mt-1 mx-1 bg-dark-background w-36">
+                {navItems.map((navItem) => (
+                  <a
+                    key={navItem.name}
+                    href={navItem.link}
+                    className=" text-gray-400 hover:text-orange-400 focus:text-orange-400 active:text-orange-700 "
+                  >
+                    {navItem.name}
+                  </a>
+                ))}
+              </span>
+            )}
+          </div>
         </button>
         <h1 className="text-orange-500 font-bold text-2xl sm:text-center md:text-left">
-          LOGO
+          <button>FAISAL</button>
         </h1>
         <div className="hidden sm:hidden md:flex">
           {navItems.map((navItem) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-background":"#121212"
+      }
+    },
+    
   },
   plugins: [],
 }
